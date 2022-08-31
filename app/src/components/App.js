f10e5d077ed9c31015c97ba38f5bffe5e46b0f08
Joshua Smith <joshua.smith@generalassemb.ly>
2022-08-31 13:39:28 -0600

    migrated react app

---
+++ app/src/components/App.js
@@ -0,0 +1,22 @@
+
+import '../styles/App.css';
+// import example: 
+//      You can import static assets directly using a variable of your choosing
+
+// import desktopImage from '../assets/images/image-header-desktop.jpg'
+
+function App() {
+  return (
+    <div className="App">
+        {/* structure your HTML + CSS rules to match the designs provided */}
+        Get insights that help your business grow. Discover the benefits of data
+     analytics and make better decisions regarding revenue, customer experience,
+        and overall efficiency. 
+        10k+ companies 
+        314 templates 
+        12m+ queries
+    </div>
+  );
+}
+
+export default App;
